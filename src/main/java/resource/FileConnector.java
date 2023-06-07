c3bbdc20e3fb53fbc3d0944e895617d328df276d
Kadiray Karakaya <kadiray.k@hotmail.com>
2023-06-07 16:03:42 +0200

    trim

--- src/main/java/resource/FileConnector.java
+++ src/main/java/resource/FileConnector.java
@@ -12,7 +12,7 @@ public class FileConnector {
     public static List<String> getMethodSignatures(String path){
         List<String> result = null;
         try (Stream<String> lines = Files.lines(Paths.get(path))) {
-            result = lines.collect(Collectors.toList());
+            result = lines.map(l->l.trim()).collect(Collectors.toList());
         }catch (IOException e){
             e.printStackTrace();
         }
